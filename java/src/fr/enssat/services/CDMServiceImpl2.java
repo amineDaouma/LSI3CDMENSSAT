1344b303a7f4c4db742a1880cd10cf39315e3ad9
Vérité <veritehugo@gmail.com>
2015-03-03 08:50:09 +0100

    Fonctions service implémentant les interfaces

---
+++ java/src/fr/enssat/services/CDMServiceImpl2.java
@@ -0,0 +1,44 @@
+package fr.enssat.services;
+
+import java.util.List;
+
+import fr.enssat.beans.CDM;
+
+public class CDMServiceImpl2 implements CDMService2
+{
+
+	@Override
+	public List<CDM> findAll() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CDM findByID(String idCDM) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CDM updateCDM(String idCDM, CDM newCDM) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addCDM(CDM newCDM) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void supprimeCDM(String idCDM) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
