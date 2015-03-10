faaac4fabf7f3fad43ce5d79431025d85c38389a
Zhang Xuyang <xuzhang@RALPH-28.enssat.fr>
2015-03-10 17:14:56 +0100

    méthodes subProgram

--- java/src/fr/enssat/dao/ProgramDAOImpl.java
+++ java/src/fr/enssat/dao/ProgramDAOImpl.java
@@ -1,101 +1,93 @@
 package fr.enssat.dao;
 
-import java.util.List;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.classic.Session;
-
 import fr.enssat.beans.CDM;
 import fr.enssat.beans.Course;
+import fr.enssat.beans.OrgUnit;
 import fr.enssat.beans.Program;
 import fr.enssat.beans.SubProgram;
 
-public class ProgramDAOImpl implements ProgramDAO 
-{
+public class ProgramDAOImpl implements ProgramDAO {
 
 	@Override
-	public Program getProgram(String idCDM) 
-	{
-//		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-//		Session session = sessionFactory.openSession();
-//		session.beginTransaction();
-//		List<Program> programList = session.createQuery("from program").list();
-//		for (Program program : programList) {
-//			if(program.getProgramID().equals(idCDM)){
-//						return program;
-//			}
-//		}
-//		return null;
-		
-		Session session = CdmDAO2Impl.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		List<Program> retour = (List<Program>) (session.createQuery("from Program").list());
-		for(Program program : retour)
-		{
-			if (program.getProgramID().equals(idCDM))
-			{
-				return program;
-			}
-		}
+	public Program getProgram(String idCDM) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Program updateProgram(String idCDM, Program newProgram) {
-//		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-//		Session session = sessionFactory.openSession();
-//		session.beginTransaction();
-//		
-//		Program oldProgramm=getProgram(idCDM);
-//		if(oldProgramm!=null){
-//			newProgram.setHjid(oldProgramm.getHjid());			
-//			session.saveOrUpdate(newProgram);				
-//			session.getTransaction().commit();			
-//			session.close();
-//			sessionFactory.close();
-//			return newProgram;			
-//		}
-//		return null;
-		
-		Session session = CdmDAO2Impl.getSessionFactory().getCurrentSession();
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SubProgram getSubProgram(String idCDM, String subProgramID) {
+		Program program=getProgram(idCDM);
+		List<SubProgram> subPrograms=program.getSubProgram();
+		for (SubProgram subProgram : subPrograms) {
+			if(subProgram.getId().equals(subProgramID)){
+				return subProgram;				
+			}
+		}		
+	}
+
+	@Override
+	public SubProgram addSubProgram(String idCDM, SubProgram subProgram) {
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		List<Program> retour = (List<Program>) (session.createQuery("from Program").list());
-		for(Program program : retour)
-		{
-			if (program.getProgramID().equals(idCDM))
-			{
-				newProgram.setHjid(program.getHjid());
-				session.merge(newProgram);
-				session.getTransaction().commit();
-				return newProgram;
+		
+		Program program=getProgram(idCDM);
+		
+		if(program!=null){
+			session.save(subProgram);		
+			session.getTransaction().commit();		
+			session.close();
+			sessionFactory.close();
+			return subProgram;
+		}
+		return null;
+	}
+
+	@Override
+	public SubProgram updateSubProgram(String idCDM, String idSubProgram,
+			SubProgram newSubProgram) {
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		Program program=getProgram(idCDM);
+		List<SubProgram> subPrograms=program.getSubProgram();
+		for (SubProgram subProgram : subPrograms) {
+			if(subProgram.getId().equals(idSubProgram)){
+				newSubProgram.setHjid(subProgram.getHjid());			
+				session.saveOrUpdate(newSubProgram);				
+				session.getTransaction().commit();			
+				session.close();
+				sessionFactory.close();
+				return newSubProgram;
 			}
 		}
 		return null;
 	}
 
 	@Override
-	public SubProgram getSubProgram(String idCDM, String subProgramID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public SubProgram addSubProgram(String idCDM, SubProgram subProgram) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Program updateSubProgram(String idCDM, String idSubProgram,
-			SubProgram newSubProgram) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public SubProgram deleteSubProgram(String idCDM, String idSubProgram) {
-		// TODO Auto-generated method stub
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		Program program=getProgram(idCDM);
+		List<SubProgram> subPrograms=program.getSubProgram();
+		for (SubProgram subProgram : subPrograms) {
+			if(subProgram.getId().equals(idSubProgram)){		
+				session.delete(subProgram);				
+				session.getTransaction().commit();			
+				session.close();
+				sessionFactory.close();
+				return subProgram;
+			}
+		}
 		return null;
 	}
 
