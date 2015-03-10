e7040f3f4d6c63db8a434b03071760fb0062583b
HVerite <veritehugo@gmail.com>
2015-03-10 17:55:07 +0100

    Update ProgramDAOImpl.java

--- java/src/fr/enssat/dao/ProgramDAOImpl.java
+++ java/src/fr/enssat/dao/ProgramDAOImpl.java
@@ -5,30 +5,31 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.classic.Session;
-import fr.enssat.beans.CDM;
-import fr.enssat.beans.Course;
-import fr.enssat.beans.OrgUnit;
+
 import fr.enssat.beans.Program;
 import fr.enssat.beans.SubProgram;
 
-public class ProgramDAOImpl implements ProgramDAO {
+public class ProgramDAOImpl implements ProgramDAO 
+{
 
 	@Override
-	public Program getProgram(String idCDM) {
+	public Program getProgram(String idCDM) 
+	{	
 		Session session = CdmDAO2Impl.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
- 		List<Program> retour = (List<Program>) (session.createQuery("from Program").list());
+		List<Program> retour = (List<Program>) (session.createQuery("from Program").list());
 		for(Program program : retour)
 		{
 			if (program.getProgramID().equals(idCDM))
 			{
-			  return program;
+				return program;
 			}
 		}
+		return null;
 	}
 
 	@Override
-	public Program updateProgram(String idCDM, Program newProgram) {
+	public Program updateProgram(String idCDM, Program newProgram) {	
 		Session session = CdmDAO2Impl.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		List<Program> retour = (List<Program>) (session.createQuery("from Program").list());
@@ -42,6 +43,7 @@ public class ProgramDAOImpl implements ProgramDAO {
 				return newProgram;
 			}
 		}
+		return null;
 	}
 
 	@Override
@@ -52,7 +54,8 @@ public class ProgramDAOImpl implements ProgramDAO {
 			if(subProgram.getId().equals(subProgramID)){
 				return subProgram;				
 			}
-		}		
+		}
+		return null;
 	}
 
 	@Override
